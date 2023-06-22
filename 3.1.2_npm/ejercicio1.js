3f7018d289f539772602a5633e72faf10507e0ca
sattara1985 <sattara1985@gmail.com>
2023-06-22 12:12:28 -0500

    semaforo

---
+++ 3.1.2_npm/ejercicio1.js
@@ -0,0 +1,9 @@
+let respuesta ="sin";
+
+if(respuesta ==="si" || respuesta === "Si" || respuesta ==="SI")
+{
+    console.log("ciertamnente");
+
+}else{
+    console.log("lo sospechaba");
+}
---
+++ 3.1.2_npm/index.js
@@ -0,0 +1,12 @@
+const PI=3.1416;
+
+//const r=prompt("dame el valor del radio"); investigar
+
+const r=2;
+let areaCirculo= PI*(r**2);
+
+//document.writeln(areaCirculo);
+//alert(areaCirculo);
+console.log(areaCirculo);
+
+//como mandar entradas a node js
